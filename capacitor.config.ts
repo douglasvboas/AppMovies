52abc460268b01fa9101e2d02e5295acf990479c
Douglas vilas boas <douglasvboas@yahoo.com.br>
2025-05-28 16:27:24 -0300

    feat: ajustes em arq de programs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.dvbtecnology.appMovie',
-  appName: 'app',
+  appName: 'Meus Filmes',
   webDir: 'www',
  // server: {
  //   url: 'http://192.168.1.6:8100',
